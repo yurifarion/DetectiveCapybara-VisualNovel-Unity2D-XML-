f99a62c5e7b06ad1f531c488e1e7188423a1391b
yuriruf <yurithedeveloper@gmail.com>
2021-04-29 15:52:06 -0300

    Detective Capybara version 0.0

---
+++ Unity/Detective Capybara/Assets/script/DialogueBox.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class DialogueBox : MonoBehaviour
+{
+	private Image dialogueBox_bg;
+	private Text dialogueText;
+	private string message;
+	private bool isTextComplete = false;
+	public float typeInterval = 0.02f;
+	public string Speaker = "None";//the character responsable for the lines
+	private bool flip_X = false;//Indicates if the speaker is on left or right, by defaul flip_x false is pointing left
+	
+	
+
+	public void Init(string nameSpeaker,string msg,bool isFlip_x){
+		Speaker = nameSpeaker;
+		flip_X = isFlip_x;
+		// if the dialog box was created without text if picks from the GameObject
+		if(msg == "")msg = dialogueText.text;
+		message = msg;
+		dialogueBox_bg = GetComponent<Image>();//get the image of the Chatbox from the gameobject
+		dialogueText = this.gameObject.transform.GetChild(0).gameObject.GetComponent<Text>();//get the Text component of the child
+		StartCoroutine(type());
+	}
+	//type the message of the chatbox respecting the typeInterval
+	IEnumerator type(){
+		dialogueText.text = "";
+		foreach(char letter in message.ToCharArray()){
+			dialogueText.text += letter;
+			yield return 0;
+			yield return new WaitForSeconds(typeInterval);
+		}
+		isTextComplete = true;
+	}
+	
+}
---
+++ Unity/Detective Capybara/Assets/script/DialogueManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+	public List<GameObject> dialogues = new List<GameObject>();//list of all dialogues this manager will deal with
+	private int dialogueOrder = 0;
+	public GameObject prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+		//create dialogue boxes
+        GameObject n = Instantiate(prefab,transform.position,transform.rotation);
+		n.GetComponent<DialogueBox>().Init("Capybara","Wait I know you ...",false);
+		n.transform.SetParent(GameObject.FindGameObjectWithTag("Canva").transform);// in order to the gameobject appear in the game it need to be a child of Canvas
+		dialogues.Add(n);
+		
+		GameObject f = Instantiate(prefab,transform.position,transform.rotation);
+		f.GetComponent<DialogueBox>().Init("Capybara","I used to be an adverture like you",false);
+		f.transform.SetParent(GameObject.FindGameObjectWithTag("Canva").transform);
+		dialogues.Add(f);
+		
+		ShowDialogue();
+    }
+	void ShowDialogue(){
+		
+		//Make all dialogues box disabled and just enable the one in the order
+		foreach(GameObject p in dialogues){
+			p.SetActive(false);
+		}
+		dialogues[dialogueOrder].SetActive(true);
+	}
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)){
+			dialogueOrder++;
+			//ShowDialogue();
+		}
+    }
+}
